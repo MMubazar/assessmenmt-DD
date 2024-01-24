869715de4aab2f55e5cd23ded9b015f02ca5889c
Muhammad Mubazar Qureshi <121640226+MMubazar@users.noreply.github.com>
2024-01-24 13:18:38 +0500

    up-to-date

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Wrapper from "./Pages/Wrapper.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Components/Styles/style.css";
 import "./Components/Styles/animate.css";
-import "./Components/Styles/pe-icon-7-stroke.css";
 import "./Components/Styles/style-magnific-popup.css";
 
 function App() {
